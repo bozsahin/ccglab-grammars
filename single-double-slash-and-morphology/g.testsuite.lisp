3544fe51a5eca80be3fd71c8bf5367943abdcbaf
Cem Bozsahin <cem.bozsahin@gmail.com>
2017-08-22 12:37:03 +0300

    one more example, with -ma

--- single-double-slash-and-morphology/g.testsuite.lisp
+++ single-double-slash-and-morphology/g.testsuite.lisp
@@ -3,15 +3,16 @@
 (2 (adam -in oku -yan kitap))
 (3 (kitap -i oku -yan adam))
 (4 (kitap -i oku -dugu adam))
+(5 (uyu -ma -yan adam)) ; 2 analyses for this one, one phrasal one lexical, same lf
 ))
 
-(defun  ders-test() ;; derivations test--going through all of them
+(defun  deriv-test() ;; derivations test--going through all of them
   (dolist (p *data*)(progn (ccg-deduce (second p))
-			   (format t "~%=======~%~s~%========~%" (first p))
+			   (format t "~%=======~%~s~%~s~%========~%" (first p)(second p))
 			   (cky-show-deduction))))
 
 (defun  lf-test() ;; verifying lfs for well formedness --- adequacy is up to you
   (dolist (p *data*)
     (progn (ccg-deduce (second p))
-	   (format t "~%=======~%~s~%========~%" (first p))
+	   (format t "~%=======~%~s~%~s~%========~%" (first p)(second p))
 	   (cky-show-lf-eqv))))
