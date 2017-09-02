9c16f6816d571dc380e19331c3ff62c7021cf29e
Cem Bozsahin <cem.bozsahin@gmail.com>
2017-09-02 11:47:00 +0300

    added a testsuite for the thought experiment

--- noqnoc/testsuite.lisp
+++ noqnoc/testsuite.lisp
@@ -4,8 +4,8 @@
 
 (defun test-noqnoc ()
   (progn
-    (lg "g1" :maker 'sbcl)  ; creates and loads the .ded file from .ccg
-    (sg "g1.ind")           ; saves currently loaded grammar as .ind
-    (um "g1" 10 1.0 1.0 :load t)  ; updates g1.ind after loading it, usiing g1.sup
+    (lg "g1")               ; loads the .ded file 
+    (sg "g1.ind")           ; saves currently loaded grammar as .ind-- for modeling
+    (um "g1" 10 1.0 1.0 :load t)  ; updates g1.ind after loading it, using g1.sup
     (st)			; shows training
     ))
