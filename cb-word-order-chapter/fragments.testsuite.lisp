61544315478f5f0799624bfd830d3dc61cea0077
bozsahin <cem.bozsahin@gmail.com>
2018-06-25 09:30:25 +0300

    revised example indices and one analysis (-an)

--- cb-word-order-chapter/fragments.testsuite.lisp
+++ cb-word-order-chapter/fragments.testsuite.lisp
@@ -2,6 +2,7 @@
 ;; then do (do-derivations) to see the derivations.
 ;; (check-lfs) will verify the lambda reductions in case you are interested.
 ;; example numbers are from the paper; see fragments.ccg header.
+;;  Interpreting the results of commented out examples needs another paper.
 ;; -cem bozsahin, May 2018
 (defparameter *testdata* '(
   (4 (Can kediyi ve Ayşe köpeğe okşadı))
@@ -10,27 +11,31 @@
   (fig1a (Mary persuades John to hit the target))
   (fig1b (Mary promises John to hit the target))
   (fig1c (Mary expects John to hit the target))
+;  (16a (the target which Mary knows that John believes that Harry spotted))
+;  (16b (the books which I think that you have read without understanding))
+;  (16c (the books which John believes and which Mary knows might skyrocket the
+;	    sales))
   (21a (give Mary books and Klaus cds))
   (21b (Mary likes and John  hates cats))
   (22 (Mary persuades John to hit the target))
-  (31a (dynes welodd gath))
-  (31b (dynes welodd cath))
+  (33a (dynes welodd gath))
+  (33b (dynes welodd cath))
   (fig2a (arı sok -an kız))
   (fig2b ("arı sok" -an kız))
   (fig3 (kızı her arı soktu))
-  (37a (soktu arı kızı))
-  (37b (soktu kızı arı))
-  (fig4 (bütün arılar soktu kızı))
-  (43a (Eu não vi "os carros"))
-  (43b (Eu não "os carros" vi))
-  (43c (Eu não os vi))
-  (43d (Eu os vi))
-  (43e (Eu não vi os))
-  (43e2 (Eu vi os))
+  (fig4a (soktu arı kızı))
+  (fig4b (soktu kızı arı))
+  (fig5 (bütün arılar soktu kızı))
+  (44a (Eu não vi "os carros"))
+  (44b (Eu não "os carros" vi))
+  (44c (Eu não os vi))
+  (44d (Eu os vi))
+  (44ee1 (Eu não vi os))
+  (43ee2 (Eu vi os))
   (fig5a ("O Paulo" não os viu))
-  (fig5b (todos os viram))
-  (fig5c (Eu não vi "os carros"))
-  (47 ("A sopa" "O Paulo" comeu))
+  (fig6b (todos os viram))
+  (fig6c (Eu não vi "os carros"))
+  (48 ("A sopa" "O Paulo" comeu))
   ))
 
 (defun  do-derivations ()
